0142d841746ceb49f40a6a21e70f31366bcbacb6
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-02-09 10:31:32 +0530

    config.py load_config updated

--- utils/config.py
+++ utils/config.py
@@ -6,8 +6,11 @@ import os
 import yaml
 
 
-def load_config(config_file="config.yaml"):
+def load_config(config_file="config.yaml", key=None):
     with open(config_file, "r") as file:
         config = yaml.safe_load(file)
 
-    return config
+    if key:
+        return config.get(key, {})
+    else:
+        return config
