fc248270cee91dbb1e7e1b497edab356a123c298
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-02-15 16:05:49 +0530

    exp_summary.py added

---
+++ workflows/exp_summary.py
@@ -0,0 +1,20 @@
+"""
+exp_summary.py
+"""
+
+import os
+import sys
+
+sys.path.append(os.getcwd())
+from utils.config import load_config
+from utils.trainer import ModelSummary
+
+
+if __name__ == "__main__":
+    args = load_config(config_file="config.yaml", key="trainer")
+
+    ms = ModelSummary(
+        exp_base_dir=args["exp_base_dir"],
+        exp_name=args["model_args"]["exp_name"],
+    )
+    results = ms.get_cv_results()
